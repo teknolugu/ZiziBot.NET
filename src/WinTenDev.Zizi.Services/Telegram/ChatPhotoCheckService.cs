5ecdc3ca74b43696008ee507866ec840012bc3e9
Azhe-kun <azhe403@gmail.com>
2021-12-29 18:29:20 +0700

    add missing param args chat id

--- src/WinTenDev.Zizi.Services/Telegram/ChatPhotoCheckService.cs
+++ src/WinTenDev.Zizi.Services/Telegram/ChatPhotoCheckService.cs
@@ -28,7 +28,7 @@ public class ChatPhotoCheckService
 
     public async Task<bool> CheckChatPhoto(long chatId, long userId, Func<CallbackAnswer, Task> funcCallbackAnswer = null)
     {
-        var op = Operation.Begin("Check Chat Photo on ChatId {ChatId} for UserId: {UserId}", userId);
+        var op = Operation.Begin("Check Chat Photo on ChatId {ChatId} for UserId: {UserId}", chatId, userId);
         var userProfilePhotos = await _chatService.GetChatPhotoAsync(userId);
         var hasPhoto = userProfilePhotos.TotalCount > 0;
 
