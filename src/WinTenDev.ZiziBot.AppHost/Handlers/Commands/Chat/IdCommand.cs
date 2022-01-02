e7a3c979fca6be7a7f7253b8c9443fffad0d3249
Azhe-kun <azhe403@gmail.com>
2022-01-03 05:40:01 +0700

    include sudo check for /id

--- src/WinTenDev.ZiziBot.AppHost/Handlers/Commands/Chat/IdCommand.cs
+++ src/WinTenDev.ZiziBot.AppHost/Handlers/Commands/Chat/IdCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using SerilogTimings;
 using Telegram.Bot.Framework.Abstractions;
 using WinTenDev.Zizi.Services.Telegram;
 using WinTenDev.Zizi.Utils;
@@ -6,63 +7,57 @@ using WinTenDev.Zizi.Utils.Telegram;
 
 namespace WinTenDev.ZiziBot.AppHost.Handlers.Commands.Chat;
 
-/// <summary>
-/// Get simple identity of Chat and User
-/// </summary>
 public class IdCommand : CommandBase
 {
     private readonly TelegramService _telegramService;
+    private readonly PrivilegeService _privilegeService;
 
-    /// <summary>
-    /// ID Constructor
-    /// </summary>
-    /// <param name="telegramService"></param>
-    public IdCommand(TelegramService telegramService)
+    public IdCommand(
+        TelegramService telegramService,
+        PrivilegeService privilegeService
+    )
     {
         _telegramService = telegramService;
+        _privilegeService = privilegeService;
     }
 
-    /// <summary>
-    /// Handle get ID command
-    /// </summary>
-    /// <param name="context"></param>
-    /// <param name="next"></param>
-    /// <param name="args"></param>
-    /// <param name="cancellationToken"></param>
     public override async Task HandleAsync(IUpdateContext context, UpdateDelegate next, string[] args)
     {
         await _telegramService.AddUpdateContext(context);
 
-        var msg = _telegramService.Message;
+        var chatTitle = _telegramService.Chat.Title;
+        var chatId = _telegramService.ChatId;
+        var chatType = _telegramService.Chat.Type;
+        var userId = _telegramService.FromId;
+        var fullName = _telegramService.From.GetFullName();
+        var userLang = _telegramService.From?.LanguageCode;
 
-        var chatTitle = msg.Chat.Title;
-        var chatId = msg.Chat.Id;
-        var chatType = msg.Chat.Type;
+        var op = Operation.Begin("Command '/id' on ChatId '{ChatId}'", chatId);
 
-        var userId = msg.From.Id;
-        var username = msg.From.Username;
-        var name = msg.From.GetFullName();
-        var userLang = msg.From.LanguageCode;
+        var fromSudo = _telegramService.IsFromSudo;
 
         var text = $"👥 <b>{chatTitle}</b>\n" +
-                   $"ID: <code>{chatId}</code>\n" +
+                   $"Chat ID: <code>{chatId}</code>\n" +
                    $"Type: <code>{chatType}</code>\n\n" +
-                   $"👤 <b>{name}</b>\n" +
-                   $"ID: <code>{userId}</code>\n" +
-                   $"Username: @{username}\n" +
-                   $"Language: {userLang.ToUpperCase()}";
+                   $"👤 <b>{fullName}</b>\n" +
+                   $"Is Sudo: <code>{fromSudo}</code>\n" +
+                   $"User ID: <code>{userId}</code>\n" +
+                   $"Language: <code>{userLang.ToUpperCase()}</code>";
 
-        if (msg.ReplyToMessage != null)
+        if (_telegramService.ReplyToMessage != null)
         {
-            var repMsg = msg.ReplyToMessage;
+            var repMsg = _telegramService.ReplyToMessage;
+            var repToFromId = _telegramService.ReplyFromId;
             var repFullName = repMsg.From.GetFullName();
+            var repToSudo = _privilegeService.IsFromSudo(repToFromId);
 
             text += $"\n\n👤 <b>{repFullName}</b>" +
-                    $"\nID: <code>{repMsg.From.Id}</code>" +
-                    $"\nUsername: @{repMsg.From.Username}" +
-                    $"\nLanguage: {repMsg.From.LanguageCode}";
+                    $"\nIs Sudo: <code>{repToSudo}</code>" +
+                    $"\nUser ID: <code>{repToFromId}</code>" +
+                    $"\nLanguage: <code>{repMsg.From?.LanguageCode}</code>";
         }
 
         await _telegramService.SendTextMessageAsync(text);
+        op.Complete();
     }
 }
