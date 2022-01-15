a896f34134f7d7c20dc8ea9be98711562f30ad1a
Azhe-kun <azhe403@gmail.com>
2022-01-16 00:42:10 +0700

    add async validation

--- src/WinTenDev.Zizi.Utils/FluentValidationUtil.cs
+++ src/WinTenDev.Zizi.Utils/FluentValidationUtil.cs
@@ -1,16 +1,24 @@
-﻿using FluentValidation;
+﻿using System.Threading.Tasks;
+using FluentValidation;
 using FluentValidation.Results;
 
+// Reference: https://github.com/baldricloth/FluentValidation.Extensions.QuickValidate/blob/master/FluentValidation.Extensions.QuickValidate/ValidationExtension.cs
 namespace WinTenDev.Zizi.Utils;
 
 public static class FluentValidationUtil
 {
     public static ValidationResult Validate<TValidator, TObj>(this TObj toValidate) where TValidator : class, new() where TObj : class
     {
-        // Reference: https://github.com/baldricloth/FluentValidation.Extensions.QuickValidate/blob/master/FluentValidation.Extensions.QuickValidate/ValidationExtension.cs
-
         var validatorInstance = new TValidator() as AbstractValidator<TObj>;
         var results = validatorInstance?.Validate(toValidate);
+
+        return results;
+    }
+
+    public static async Task<ValidationResult> ValidateAsync<TValidator, TObj>(this TObj toValidate) where TValidator : class, new() where TObj : class
+    {
+        var validatorInstance = new TValidator() as AbstractValidator<TObj>;
+        var results = await validatorInstance?.ValidateAsync(toValidate)!;
 
         return results;
     }
