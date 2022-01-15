39ecfa2894baf6e17dad09eb5b0d35b9a4a8a9f4
Azhe-kun <azhe403@gmail.com>
2022-01-15 20:45:33 +0700

    support Unified verification

--- src/WinTenDev.ZiziBot.AppHost/Handlers/Callbacks/VerifyCallback.cs
+++ src/WinTenDev.ZiziBot.AppHost/Handlers/Callbacks/VerifyCallback.cs
@@ -1,31 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Serilog;
-using Telegram.Bot;
 using Telegram.Bot.Types;
+using WinTenDev.Zizi.Models.Enums;
 using WinTenDev.Zizi.Models.Types;
 using WinTenDev.Zizi.Services.Internals;
 using WinTenDev.Zizi.Services.Telegram;
-using WinTenDev.Zizi.Utils;
 
 namespace WinTenDev.ZiziBot.AppHost.Handlers.Callbacks;
 
 public class VerifyCallback
 {
     private readonly TelegramService _telegramService;
-    private readonly CheckUsernameService _checkUsernameService;
+    private readonly UserProfilePhotoService _userProfilePhotoService;
+    private readonly StepHistoriesService _stepHistoriesService;
     private readonly CallbackQuery _callbackQuery;
 
     public VerifyCallback(
         TelegramService telegramService,
-        CheckUsernameService checkUsernameService
+        UserProfilePhotoService userProfilePhotoService,
+        StepHistoriesService stepHistoriesService
     )
     {
         _telegramService = telegramService;
-        _checkUsernameService = checkUsernameService;
-        _callbackQuery = telegramService.Context.Update.CallbackQuery;
+        _userProfilePhotoService = userProfilePhotoService;
+        _stepHistoriesService = stepHistoriesService;
+        _callbackQuery = telegramService.CallbackQuery;
 
         Log.Information("Receiving Verify Callback");
     }
@@ -35,88 +36,81 @@ public class VerifyCallback
         Log.Information("Executing Verify Callback");
 
         var callbackData = _callbackQuery.Data;
-        var fromId = _callbackQuery.From.Id;
-        var chatId = _callbackQuery.Message.Chat.Id;
+        var fromId = _telegramService.FromId;
+        var chatId = _telegramService.ChatId;
 
         Log.Debug("CallbackData: {CallbackData} from {FromId}", callbackData, fromId);
 
         var partCallbackData = callbackData.Split(" ");
-        var callBackParam1 = partCallbackData.ValueOfIndex(1);
+        var callBackParam1 = partCallbackData.ElementAtOrDefault(1);
         var answer = "Tombol ini bukan untukmu Bep!";
 
         Log.Debug("Verify Param1: {0}", callBackParam1);
 
-        if (callBackParam1 == "username")
+        Log.Information("Starting Verify from History for UserId: {UserId}", fromId);
+        var needVerifyList = await _stepHistoriesService.GetStepHistoryVerifyCore(new StepHistory()
         {
-            Log.Debug("Checking username");
-            if (_telegramService.IsNoUsername)
+            ChatId = chatId,
+            UserId = fromId
+        });
+
+        if (!needVerifyList.Any())
+        {
+            answer = "Kamu tidak perlu verifikasi!";
+        }
+        else
+        {
+            await _userProfilePhotoService.ResetUserProfilePhotoCacheAsync(fromId);
+
+            foreach (var step in needVerifyList)
             {
-                answer = "Sepertinya Anda belum memasang Username, silakan di periksa kembali.";
+                var updateHistory = step;
+                updateHistory.UpdatedAt = DateTime.Now;
+
+                switch (step.Name)
+                {
+                    case StepHistoryName.ChatMemberUsername:
+                        Log.Debug("Verifying Username for UserId {UserId}", fromId);
+                        if (_telegramService.HasUsername)
+                        {
+                            updateHistory.Status = StepHistoryStatus.HasVerify;
+                        }
+                        break;
+
+                    case StepHistoryName.ChatMemberPhoto:
+                        Log.Debug("Verifying User Profile Photo for UserId {UserId}", fromId);
+                        if (await _userProfilePhotoService.HasUserProfilePhotosAsync(fromId))
+                        {
+                            updateHistory.Status = StepHistoryStatus.HasVerify;
+                        }
+                        break;
+
+                    case StepHistoryName.HumanVerification:
+                        updateHistory.Status = StepHistoryStatus.HasVerify;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                await _stepHistoriesService.SaveStepHistory(updateHistory);
+            }
+
+            var afterVerify = await _stepHistoriesService.GetStepHistoryVerifyCore(new StepHistory()
+            {
+                ChatId = chatId,
+                UserId = fromId
+            });
+
+            if (!afterVerify.Any())
+            {
+                await _telegramService.UnmuteChatMemberAsync(fromId);
+                answer = "Terima kasih sudah verifikasi!";
             }
             else
             {
-                // await _telegramService.RemoveOldWarnUsernameQueue(chatId);
-
-                // var warnJson = await WarnUsernameUtil.GetWarnUsernameCollectionAsync();
-                // var listWarns = warnJson.AsQueryable().ToList();
-                var listWarns = new List<WarnUsernameHistory>();
-
-                Parallel.ForEach(listWarns, async (warn) =>
-                    // foreach (var warn in listWarns)
-                {
-                    var userId = warn.FromId;
-                    var isExist = listWarns.Any(x => x.FromId == userId);
-                    Log.Debug("Is UserId: {0} exist? => {1}", userId, isExist);
-
-                    try
-                    {
-                        var chatMember = await _telegramService.Client.GetChatMemberAsync(chatId, userId);
-
-                        if (_telegramService.IsNoUsername)
-                        {
-                            Log.Debug("User {0} does not set an Username", userId);
-                        }
-                        else
-                        {
-                            await _telegramService.RestrictMemberAsync(fromId, true);
-
-
-                            if (isExist)
-                            {
-                                // var delete = await warnJson.DeleteManyAsync(x =>
-                                //         x.FromId == userId && x.ChatId == chatId);
-                                // Log.Debug("Deleting {0} result {1}", userId, delete);
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Log.Error(ex, "Error Occured.");
-
-                        if (ex.Message.Contains("user not found"))
-                        {
-                            Log.Debug("Maybe UserID '{0}' has leave from ChatID '{1}' ", userId, chatId);
-                            if (isExist)
-                            {
-                                // var delete = await warnJson.DeleteManyAsync(x =>
-                                //         x.FromId == userId && x.ChatId == chatId);
-                                // Log.Debug("Deleting {0} result {1}", userId, delete);
-                            }
-                        }
-                    }
-
-                });
-
-                answer = "Terima kasih sudah verifikasi Username!";
+                answer = "Silakan lakukan sesuai instruksi, lalu tekan Verifikasi";
             }
-
-            // await _telegramService.UpdateWarnMessageAsync();
-            Log.Debug("Verify Username finish");
-        }
-        else if (fromId == callBackParam1.ToInt64())
-        {
-            await _telegramService.RestrictMemberAsync(callBackParam1.ToInt(), true);
-            answer = "Terima kasih sudah verifikasi!";
         }
 
         await _telegramService.AnswerCallbackQueryAsync(answer);
