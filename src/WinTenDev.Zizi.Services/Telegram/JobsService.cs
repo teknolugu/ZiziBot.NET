4f5acbecd4e1c61e2a2a6c6a5010684a5ca9b231
Azhe-kun <azhe403@gmail.com>
2022-01-15 18:40:14 +0700

    move HangfireJobsService

---
+++ src/WinTenDev.Zizi.Services/Telegram/JobsService.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.Extensions.Options;
+using RepoDb;
+using Serilog;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using WinTenDev.Zizi.Models.Configs;
+using WinTenDev.Zizi.Models.Enums;
+using WinTenDev.Zizi.Models.Types;
+using WinTenDev.Zizi.Services.Internals;
+using WinTenDev.Zizi.Utils;
+
+namespace WinTenDev.Zizi.Services.Telegram;
+
+public class JobsService
+{
+    private readonly EventLogConfig _eventLogConfig;
+    private readonly StepHistoriesService _stepHistoriesService;
+    private readonly ChatService _chatService;
+    private readonly TelegramBotClient _botClient;
+
+    public JobsService(
+        IOptionsSnapshot<EventLogConfig> eventLogConfig,
+        StepHistoriesService stepHistoriesService,
+        ChatService chatService,
+        TelegramBotClient botClient
+    )
+    {
+        _eventLogConfig = eventLogConfig.Value;
+        _stepHistoriesService = stepHistoriesService;
+        _chatService = chatService;
+        _botClient = botClient;
+    }
+
+    [JobDisplayName("Member Kick Job {1}@{0}")]
+    [AutomaticRetry(Attempts = 1, OnAttemptsExceeded = AttemptsExceededAction.Delete)]
+    public async Task MemberKickJob(
+        long chatId,
+        long userId
+    )
+    {
+        var needVerify = await _stepHistoriesService.GetStepHistoryVerifyCore(new StepHistory()
+        {
+            ChatId = chatId,
+            UserId = userId
+        });
+
+        if (!needVerify.Any()) return;
+
+        Log.Information("Starting kick member for UserId {UserId} from {ChatId}", userId, chatId);
+
+        var history = needVerify.FirstOrDefault();
+
+        if (history == null) return;
+
+        await _botClient.BanChatMemberAsync(chatId, userId);
+        await _botClient.UnbanChatMemberAsync(chatId, userId);
+        if (history.LastWarnMessageId != -1)
+            await _botClient.DeleteMessageAsync(chatId, history.LastWarnMessageId);
+
+        await UpdateStepHistoryStatus(chatId, userId);
+
+        var chat = await _chatService.GetChatAsync(chatId);
+
+        var sb = new StringBuilder()
+            .Append("<b>Action:</b> #KickMember").AppendLine()
+            .Append("<b>User:</b> ").AppendFormat("[<code>{0}</code>] {1} {2}\n", userId.ToString(), history.FirstName, history.LastName)
+            .Append("<b>Chat:</b> ").AppendFormat("[<code>{0}</code>] {1} \n", chatId.ReduceChatId(), chat.Title)
+            .Append("<b>Reason:</b> ").AppendJoin(",", needVerify.Select(x => $"#{x.Name}"));
+
+        await _botClient.SendTextMessageAsync(_eventLogConfig.ChannelId, sb.ToTrimmedString(), parseMode: ParseMode.Html);
+
+        Log.Information("UserId {UserId} successfully kicked from {ChatId}", userId, chatId);
+    }
+
+    [AutomaticRetry(Attempts = 2, LogEvents = true, OnAttemptsExceeded = AttemptsExceededAction.Delete)]
+    public async Task SendEventLog(
+        long chatId,
+        string messageText
+    )
+    {
+        var chatIds = new List<long>()
+        {
+            chatId,
+            _eventLogConfig.ChannelId
+        };
+
+        foreach (var targetChatId in chatIds)
+        {
+            await _botClient.SendTextMessageAsync(targetChatId, messageText);
+        }
+    }
+
+    public async Task<int> UpdateStepHistoryStatus(
+        long chatId,
+        long userId
+    )
+    {
+        var fields = Field.Parse<StepHistory>(history => new
+        {
+            history.Status,
+            history.UpdatedAt
+        });
+
+        var result = await _stepHistoriesService.UpdateStepHistoryStatus(new StepHistory
+        {
+            ChatId = chatId,
+            UserId = userId,
+            Status = StepHistoryStatus.ActionDone,
+            UpdatedAt = DateTime.Now
+        }, fields);
+
+        return result;
+    }
+}
