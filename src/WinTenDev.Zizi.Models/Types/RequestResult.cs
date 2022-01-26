310f3df5535c597dc5c199818cef9a4ef87090b9
Azhe-kun <azhe403@gmail.com>
2022-01-26 23:43:06 +0700

    add more properties

--- src/WinTenDev.Zizi.Models/Types/RequestResult.cs
+++ src/WinTenDev.Zizi.Models/Types/RequestResult.cs
@@ -1,8 +1,14 @@
-﻿namespace WinTenDev.Zizi.Models.Types;
+﻿using System;
+using Telegram.Bot.Types;
+
+namespace WinTenDev.Zizi.Models.Types;
 
 public class RequestResult
 {
     public bool IsSuccess { get; set; }
     public int ErrorCode { get; set; }
     public string ErrorMessage { get; set; }
+    public Exception ErrorException { get; set; }
+    public Message SentMessage { get; set; }
+    public Message[] SentMessages { get; set; }
 }
