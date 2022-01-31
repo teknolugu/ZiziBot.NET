8ef800d0846890302420843af886595e3bba087f
Azhe-kun <azhe403@gmail.com>
2022-02-01 02:46:41 +0700

    fix code style

--- src/WinTenDev.Zizi.Utils/Text/CsvUtil.cs
+++ src/WinTenDev.Zizi.Utils/Text/CsvUtil.cs
@@ -14,17 +14,25 @@ namespace WinTenDev.Zizi.Utils.Text;
 public static class CsvUtil
 {
     /// <summary>
-    /// Writes the.
+    /// Writes the records into CSV
     /// </summary>
-    /// <param name="filePath">The file path.</param>
     /// <param name="records">The records.</param>
+    /// <param name="filePath">The file path.</param>
     /// <param name="delimiter">The delimiter.</param>
     /// <param name="append">If true, append.</param>
-    public static void WriteRecords<T>(this List<T> records, string filePath, string delimiter = ",", bool append = false)
+    public static void WriteRecords<T>(
+        this List<T> records,
+        string filePath,
+        string delimiter = ",",
+        bool append = false
+    )
     {
-        Log.Information("Writing {0} rows to {1}", records.Count, filePath);
+        Log.Information
+        (
+            "Writing {Count} rows to {FilePath}",
+            records.Count, filePath
+        );
 
-        // v20 or above
         var config = new CsvConfiguration(CultureInfo.CurrentCulture)
         {
             Delimiter = delimiter,
@@ -42,23 +50,28 @@ public static class CsvUtil
         var csv = new CsvWriter(writer, config);
 
         csv.WriteRecords(records);
-        Log.Debug("CSV file written to {0}", filePath);
+        Log.Debug("CSV file written to {FilePath}", filePath);
 
         writer.Dispose();
         csv.Dispose();
     }
 
     /// <summary>
-    /// Write single record.
+    /// Write single record into CSV file
     /// </summary>
     /// <param name="record">The record.</param>
     /// <param name="filePath">The file path.</param>
     /// <param name="delimiter">The delimiter.</param>
     /// <param name="hasHeader"></param>
     /// <returns>A string.</returns>
-    public static string WriteRecord<T>(this T record, string filePath, string delimiter = ",", bool hasHeader = false)
+    public static string WriteRecord<T>(
+        this T record,
+        string filePath,
+        string delimiter = ",",
+        bool hasHeader = false
+    )
     {
-        Log.Information("Writing record to {0}", filePath);
+        Log.Information("Writing record to {FilePath}", filePath);
 
         // v20 or above
         var config = new CsvConfiguration(CultureInfo.CurrentCulture)
@@ -71,21 +84,25 @@ public static class CsvUtil
         using var csv = new CsvWriter(writer, config);
 
         csv.WriteRecord(record);
-        Log.Debug("CSV file written to {0}", filePath);
+        Log.Debug("CSV file written to {FilePath}", filePath);
 
         return filePath;
     }
 
     /// <summary>
-    /// Appends the row.
+    /// Appends the record into CSV file
     /// </summary>
     /// <param name="row">The row.</param>
     /// <param name="filePath">The file path.</param>
     /// <param name="delimiter">The delimiter.</param>
     /// <returns>A string.</returns>
-    public static string AppendRecord<T>(this T row, string filePath, string delimiter = ",")
+    public static string AppendRecord<T>(
+        this T row,
+        string filePath,
+        string delimiter = ","
+    )
     {
-        Log.Information("Append record to {0}", filePath);
+        Log.Information("Append record to {FilePath}", filePath);
 
         // v20 or above
         var config = new CsvConfiguration(CultureInfo.CurrentCulture)
@@ -100,27 +117,30 @@ public static class CsvUtil
 
         csv.WriteField(row);
 
-        Log.Debug("CSV file written to {0}", filePath);
+        Log.Debug("CSV file written to {FilePath}", filePath);
 
         return filePath;
     }
 
     /// <summary>
-    /// Reads the csv.
+    /// Reads the CSV file as Records
     /// </summary>
     /// <param name="filePath">The file path.</param>
     /// <param name="hasHeader">If true, has header.</param>
     /// <param name="delimiter">The delimiter.</param>
     /// <returns>A list of TS.</returns>
-    public static IEnumerable<T> ReadCsv<T>(string filePath, bool hasHeader = true, string delimiter = ",")
+    public static IEnumerable<T> ReadCsv<T>(
+        this string filePath,
+        bool hasHeader = true,
+        string delimiter = ","
+    )
     {
         if (!File.Exists(filePath))
         {
-            Log.Information("File {0} is not exist", filePath);
+            Log.Information("File {FilePath} is not exist", filePath);
             return new List<T>();
         }
 
-        // v20 or above
         var csvConfiguration = new CsvConfiguration(CultureInfo.CurrentCulture)
         {
             HasHeaderRecord = hasHeader,
@@ -134,7 +154,7 @@ public static class CsvUtil
         using var csv = new CsvReader(reader, csvConfiguration);
 
         var records = csv.GetRecords<T>().ToList();
-        Log.Information("Parsing csv records {0} record(s)", records.Count);
+        Log.Information("Parsing csv records {Count} record(s)", records.Count);
 
         return records;
     }
