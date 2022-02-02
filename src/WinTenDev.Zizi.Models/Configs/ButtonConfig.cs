4dd07159cdd16127dad7fddeeddf8bd6ae308240
Azhe-kun <azhe403@gmail.com>
2022-02-03 05:33:52 +0700

    add class ButtonConfig

---
+++ src/WinTenDev.Zizi.Models/Configs/ButtonConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using WinTenDev.Zizi.Models.Types;
+
+namespace WinTenDev.Zizi.Models.Configs;
+
+public class ButtonConfig
+{
+    public List<ButtonItem> Items { get; set; }
+}
+
+public class ButtonItem
+{
+    public string Key { get; set; }
+    public ButtonData Data { get; set; }
+}
+
+public class ButtonData
+{
+    public List<string> Descriptions { get; set; }
+    public List<string> Warnings { get; set; }
+    public List<string> Notes { get; set; }
+    public List<List<ButtonMarkup>> Buttons { get; set; }
+}
