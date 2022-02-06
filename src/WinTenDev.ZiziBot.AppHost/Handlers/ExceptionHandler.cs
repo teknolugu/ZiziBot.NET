07c48242f9d2903997dc0a09c639ac297b993b42
Azhe-kun <azhe403@gmail.com>
2022-02-07 05:46:47 +0700

    send json Update to eventLog instead of log error

--- src/WinTenDev.ZiziBot.AppHost/Handlers/ExceptionHandler.cs
+++ src/WinTenDev.ZiziBot.AppHost/Handlers/ExceptionHandler.cs
@@ -1,22 +1,37 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Serilog;
 using Telegram.Bot.Framework.Abstractions;
+using WinTenDev.Zizi.Services.Telegram;
+using WinTenDev.Zizi.Utils;
 using WinTenDev.Zizi.Utils.Text;
 
 namespace WinTenDev.ZiziBot.AppHost.Handlers;
 
 public class ExceptionHandler : IUpdateHandler
 {
-    public ExceptionHandler()
+    private readonly TelegramService _telegramService;
+
+    public ExceptionHandler(
+        TelegramService telegramService
+    )
     {
+        _telegramService = telegramService;
     }
 
-    public async Task HandleAsync(IUpdateContext context, UpdateDelegate next, CancellationToken cancellationToken)
+    public async Task HandleAsync(
+        IUpdateContext context,
+        UpdateDelegate next,
+        CancellationToken cancellationToken
+    )
     {
-        var contextUpdate = context.Update;
+        await _telegramService.AddUpdateContext(context);
+
+        var chatId = _telegramService.ChatId;
+        var update = _telegramService.Update;
 
         try
         {
@@ -24,8 +39,16 @@ public class ExceptionHandler : IUpdateHandler
         }
         catch (Exception e)
         {
-            Log.Error(e.Demystify(), "Exception handler");
-            Log.Error("{Message}. {@JsonUpdate}", e.Message, contextUpdate.ToJson(true));
+            Log.Error(e.Demystify(), "Exception handler at ChatId: {ChatId}", chatId);
+
+            var eventBuilder = new StringBuilder()
+                .Append("<b>Message: </b>").AppendLine(e.Message)
+                .AppendLine()
+                .Append("<code>")
+                .Append(update.ToJson(true))
+                .Append("</code>");
+
+            await _telegramService.SendEventLogRawAsync(eventBuilder.ToTrimmedString());
         }
     }
 }
