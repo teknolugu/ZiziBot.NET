0583626be9bc947b160d39ed17386e8410632357
Azhe-kun <azhe403@gmail.com>
2022-02-13 17:48:01 +0700

    fix logging and session file name

--- src/WinTenDev.Zizi.Utils/Extensions/TelegramServiceExtension.cs
+++ src/WinTenDev.Zizi.Utils/Extensions/TelegramServiceExtension.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Nito.AsyncEx.Synchronous;
-using Serilog;
 using WinTenDev.Zizi.Models.Configs;
 using WTelegram;
 
@@ -12,40 +12,52 @@ public static class TelegramServiceExtension
 {
     public static IServiceCollection AddWtTelegramApi(this IServiceCollection services)
     {
-        services.AddSingleton(provider => {
-            var serviceScope = provider.CreateScope().ServiceProvider;
-            var _tdLibConfig = serviceScope.GetRequiredService<IOptionsSnapshot<TdLibConfig>>().Value;
-            var _logger = serviceScope.GetRequiredService<ILogger>();
+        services.AddSingleton
+        (
+            provider => {
+                var serviceScope = provider.CreateScope().ServiceProvider;
+                var tdLibConfig = serviceScope.GetRequiredService<IOptionsSnapshot<TdLibConfig>>().Value;
+                var logger = provider.GetRequiredService<ILoggerFactory>().CreateLogger("WTelegram");
 
-            string Config(string what)
-            {
-                switch (what)
+                var apiId = tdLibConfig.ApiId;
+
+                string Config(string what)
                 {
-                    case "api_id": return _tdLibConfig.ApiId;
-                    case "api_hash": return _tdLibConfig.ApiHash;
-                    case "phone_number": return _tdLibConfig.PhoneNumber;
-                    case "verification_code":
+                    switch (what)
                     {
-                        _logger.Information("Input Verification Code: ");
-                        return Console.ReadLine();
+                        case "api_id": return apiId;
+                        case "api_hash": return tdLibConfig.ApiHash;
+                        case "phone_number": return tdLibConfig.PhoneNumber;
+                        case "verification_code":
+                        {
+                            logger.LogInformation("Input Verification Code: ");
+                            return Console.ReadLine();
+                        }
+                        case "session_pathname": return $"Storage/Common/WTelegram_session_{apiId}.dat";
+                        case "first_name": return tdLibConfig.FirstName;// if sign-up is required
+                        case "last_name": return tdLibConfig.LastName;// if sign-up is required
+                        // case "password": return "secret!";     // if user has enabled 2FA
+                        default: return null;// let WTelegramClient decide the default config
                     }
-                    case "session_pathname": return "Storage/Common/WTelegram_session.dat";
-                    case "first_name": return _tdLibConfig.FirstName;// if sign-up is required
-                    case "last_name": return _tdLibConfig.LastName;// if sign-up is required
-                    // case "password": return "secret!";     // if user has enabled 2FA
-                    default: return null;// let WTelegramClient decide the default config
                 }
+
+                Helpers.Log = (
+                    logLevel,
+                    logStr
+                ) => logger.Log((LogLevel) logLevel, "WTelegram: {S}", logStr);
+
+                var client = new Client(Config);
+                var user = client.LoginUserIfNeeded().WaitAndUnwrapException();
+
+                logger.LogInformation
+                (
+                    "We are logged-in as {0} (id {1})",
+                    user.username ?? user.first_name + " " + user.last_name, user.id
+                );
+
+                return client;
             }
-
-            Helpers.Log = (i, s) => _logger.Debug("WTelegram: {I} - {S}", i, s);
-
-            var client = new Client(Config);
-            var user = client.LoginUserIfNeeded().WaitAndUnwrapException();
-            _logger.Information("We are logged-in as {0} (id {1})",
-            user.username ?? user.first_name + " " + user.last_name, user.id);
-
-            return client;
-        });
+        );
 
         return services;
     }
