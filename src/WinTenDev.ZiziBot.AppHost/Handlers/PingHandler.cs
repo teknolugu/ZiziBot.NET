7660c54607f8f4f4ae22975ab3eefb9d50c0c3ce
Azhe-kun <azhe403@gmail.com>
2022-03-01 15:14:34 +0700

    add ping sender & response into message history

--- src/WinTenDev.ZiziBot.AppHost/Handlers/PingHandler.cs
+++ src/WinTenDev.ZiziBot.AppHost/Handlers/PingHandler.cs
@@ -5,6 +5,7 @@ using SerilogTimings;
 using Telegram.Bot;
 using Telegram.Bot.Framework.Abstractions;
 using Telegram.Bot.Types.ReplyMarkups;
+using WinTenDev.Zizi.Models.Enums;
 using WinTenDev.Zizi.Services.Telegram;
 
 namespace WinTenDev.ZiziBot.AppHost.Handlers;
@@ -18,22 +19,37 @@ internal class PingHandler : IUpdateHandler
         _telegramService = telegramService;
     }
 
-    public async Task HandleAsync(IUpdateContext context, UpdateDelegate next, CancellationToken cancellationToken)
+    public async Task HandleAsync(
+        IUpdateContext context,
+        UpdateDelegate next,
+        CancellationToken cancellationToken
+    )
     {
         var op = Operation.Begin("Ping Command handler");
 
         await _telegramService.AddUpdateContext(context);
+        var senderMessageId = _telegramService.MessageOrEdited.MessageId;
 
-        var keyboard = new InlineKeyboardMarkup(InlineKeyboardButton.WithCallbackData("Ping", "PONG"));
+        var keyboard = new InlineKeyboardMarkup(
+            InlineKeyboardButton.WithCallbackData(
+                "Ping",
+                "PONG"
+            )
+        );
 
         var sendText = "ℹ️ Pong!!";
         var isSudoer = _telegramService.IsFromSudo;
+
+        await _telegramService.SaveToMessageHistoryAsync(
+            senderMessageId,
+            MessageFlag.Ping
+        );
 
         if (_telegramService.IsPrivateChat && isSudoer)
         {
             sendText += $"\n🕔 <code>{DateTime.Now}</code>" +
                         "\n🎛 <b>Engine info.</b>";
-            var getWebHookInfo = await _telegramService.Client.GetWebhookInfoAsync();
+            var getWebHookInfo = await _telegramService.Client.GetWebhookInfoAsync(cancellationToken: cancellationToken);
             if (string.IsNullOrEmpty(getWebHookInfo.Url))
             {
                 sendText += "\n\n<i>Bot run in Poll mode.</i>";
@@ -51,7 +67,16 @@ internal class PingHandler : IUpdateHandler
             }
         }
 
-        await _telegramService.SendTextMessageAsync(sendText, keyboard);
+        var sentMessage = await _telegramService.SendTextMessageAsync(
+            sendText,
+            keyboard
+        );
+
+        await _telegramService.SaveToMessageHistoryAsync(
+            sentMessage.MessageId,
+            MessageFlag.Ping
+        );
+
         op.Complete();
     }
 }
