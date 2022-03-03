9ffd6ba19ccfe9b4d5b8bc63d0ab56281b0c174f
Azhe-kun <azhe403@gmail.com>
2022-03-03 17:21:06 +0700

    add class MessageResponseDto

---
+++ src/WinTenDev.Zizi.Models/Dto/MessageResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+
+namespace WinTenDev.Zizi.Models.Dto;
+
+public class MessageResponseDto
+{
+    public string MessageText { get; set; }
+    public IReplyMarkup ReplyMarkup { get; set; }
+    public bool DisableWebPreview { get; set; } = true;
+    public long ReplyToMessageId { get; set; }
+}
