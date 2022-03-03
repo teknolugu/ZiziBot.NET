27cdd6900540b07f21875b234427f8402f3ee237
Azhe-kun <azhe403@gmail.com>
2022-03-03 15:36:27 +0700

    add create table rules

---
+++ src/WinTenDev.Zizi.DbMigrations/FluentMigrations/CreateTableRules.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+using JetBrains.Annotations;
+using WinTenDev.Zizi.DbMigrations.Extensions;
+
+namespace WinTenDev.Zizi.DbMigrations.FluentMigrations;
+
+[Migration(120220303110401)]
+[UsedImplicitly]
+public class CreateTableRules : Migration
+{
+    private const string TableName = "rules";
+
+    public override void Up()
+    {
+        if (Schema.Table(TableName).Exists()) return;
+
+        Create.Table(TableName)
+            .WithIdColumn()
+            .WithColumn("from_id").AsInt64()
+            .WithColumn("chat_id").AsInt64()
+            .WithColumn("rule_text").AsString(4000)
+            .WithTimeStamps();
+    }
+
+    public override void Down()
+    {
+        Delete.Table(TableName);
+    }
+}
