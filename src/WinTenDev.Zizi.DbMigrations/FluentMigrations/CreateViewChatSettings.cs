c5f7f2d8b202798cce48741a772367ef0b75068a
Azhe Kun <azhe403@gmail.com>
2022-03-05 22:25:35 +0700

    add table view for chatSettings

---
+++ src/WinTenDev.Zizi.DbMigrations/FluentMigrations/CreateViewChatSettings.cs
@@ -0,0 +1,37 @@
+﻿using FluentMigrator;
+using JetBrains.Annotations;
+using WinTenDev.Zizi.DbMigrations.Extensions;
+
+namespace WinTenDev.Zizi.DbMigrations.FluentMigrations;
+
+[Maintenance(MigrationStage.AfterAll)]
+[UsedImplicitly]
+public class CreateViewChatSettings : Migration
+{
+    private const string ViewListGroups = "view_list_groups";
+    private const string ViewListPrivates = "view_list_privates";
+
+    public override void Up()
+    {
+        const string sqlListGroup = $"create or replace view {ViewListGroups} as " +
+                                    "select * from group_settings  " +
+                                    "where chat_type = 'group' || chat_type = 'supergroup' || chat_type = 'channel' " +
+                                    "order by updated_at desc;";
+
+        Execute.Sql(sqlListGroup);
+
+        const string sqlListPrivate = $"create or replace view {ViewListPrivates} as " +
+                                      "select * from group_settings  " +
+                                      "where chat_type = 'private' " +
+                                      "order by updated_at desc;";
+
+        Execute.Sql(sqlListPrivate);
+
+    }
+
+    public override void Down()
+    {
+        Execute.DropViewIfExist(ViewListGroups);
+        Execute.DropViewIfExist(ViewListPrivates);
+    }
+}
