42e54a392727fffbc3e499a901eeec01e07da103
Azhe Kun <azhe403@gmail.com>
2022-03-06 12:13:21 +0700

    fix out command

--- src/WinTenDev.ZiziBot.AppHost/Handlers/Commands/Core/OutCommand.cs
+++ src/WinTenDev.ZiziBot.AppHost/Handlers/Commands/Core/OutCommand.cs
@@ -1,8 +1,14 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Serilog;
+using Telegram.Bot;
 using Telegram.Bot.Framework.Abstractions;
+using WinTenDev.Zizi.Models.Dto;
 using WinTenDev.Zizi.Services.Telegram;
+using WinTenDev.Zizi.Services.Telegram.Extensions;
 using WinTenDev.Zizi.Utils;
+using WinTenDev.Zizi.Utils.Telegram;
 
 namespace WinTenDev.ZiziBot.AppHost.Handlers.Commands.Core;
 
@@ -23,7 +29,9 @@ public class OutCommand : CommandBase
     {
         await _telegramService.AddUpdateContext(context);
 
+        var chatId = _telegramService.ChatId;
         var partsMsg = _telegramService.MessageTextParts;
+        var client = _telegramService.Client;
 
         await _telegramService.DeleteSenderMessageAsync();
 
@@ -31,15 +39,51 @@ public class OutCommand : CommandBase
 
         var sendText = "Maaf, saya harus keluar";
 
-        if (partsMsg.ValueOfIndex(2) != null)
+        if (partsMsg.ElementAtOrDefault(2) != null)
         {
-            sendText += $"\n{partsMsg.ValueOfIndex(1)}";
+            sendText += $"\n{partsMsg.ElementAtOrDefault(2)}";
         }
 
-        var chatId = partsMsg.ValueOfIndex(1).ToInt64();
-        Log.Information("Target out: {ChatId}", chatId);
+        var targetChatId = partsMsg.ElementAtOrDefault(1).ToInt64();
+        Log.Information("Target out: {ChatId}", targetChatId);
 
-        await _telegramService.SendTextMessageAsync(sendText, customChatId: chatId, replyToMsgId: 0);
-        await _telegramService.LeaveChat(chatId);
+        var me = await _telegramService.GetMeAsync();
+        var meFullName = me.GetFullName();
+
+        try
+        {
+            if (targetChatId == 0) targetChatId = chatId;
+
+            await _telegramService.SendTextMessageAsync(
+                sendText,
+                customChatId: targetChatId,
+                replyToMsgId: 0
+            );
+
+            await client.LeaveChatAsync(targetChatId);
+
+            if (targetChatId != chatId)
+            {
+                await _telegramService.SendMessageTextAsync(
+                    new MessageResponseDto()
+                    {
+                        MessageText = $"{meFullName} berhasil keluar dari group" +
+                                      $"\nChatId: {targetChatId}",
+                        ScheduleDeleteAt = DateTime.UtcNow.AddMinutes(5)
+                    }
+                );
+            }
+        }
+        catch (Exception e)
+        {
+            await _telegramService.SendMessageTextAsync(
+                new MessageResponseDto()
+                {
+                    MessageText = $"Sepertinya {meFullName} bukan lagi anggota ChatId {targetChatId}" +
+                                  $"\nMessage: {e.Message}",
+                    ScheduleDeleteAt = DateTime.UtcNow.AddMinutes(5)
+                }
+            );
+        }
     }
 }
