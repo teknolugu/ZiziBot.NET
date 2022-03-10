e5535651e2b4c65d06d928847248c159d1132f97
Azhe-kun <azhe403@gmail.com>
2022-03-11 01:29:34 +0700

    add parser helper

---
+++ src/WinTenDev.Zizi.Utils/Parsers/GithubUtil.cs
@@ -0,0 +1,9 @@
+﻿namespace WinTenDev.Zizi.Utils.Parsers;
+
+public static class GithubUtil
+{
+    public static bool IsGithubReleaseUrl(this string url)
+    {
+        return url.Contains("github.com") && url.Contains("releases");
+    }
+}
---
+++ src/WinTenDev.Zizi.Utils/Parsers/WordPressUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Serilog;
+using WordPressPCL;
+
+namespace WinTenDev.Zizi.Utils.Parsers;
+
+public static class WordPressUtil
+{
+    public static async Task<bool> IsWordpress(this string url)
+    {
+        try
+        {
+            var uri = url.ParseUrl();
+            var client = new WordPressClient(uri.Host);
+            var post = await client.Posts.GetAll();
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            Log.Warning("Unable to check if {Url} is a wordpress site", url);
+            return false;
+        }
+    }
+}
