f7282fdce70eccdd975860a0e473ac3e4596d520
Azhe-kun <azhe403@gmail.com>
2022-03-18 18:29:23 +0700

    reading build date

---
+++ src/WinTenDev.Zizi.Models/Attributes/BuildDateAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace WinTenDev.Zizi.Models.Attributes;
+
+[AttributeUsage(AttributeTargets.Assembly)]
+public class BuildDateAttribute : Attribute
+{
+    public DateTime BuildDate { get; set; }
+
+    public BuildDateAttribute(string value)
+    {
+        BuildDate = DateTime.ParseExact(
+            value,
+            "yyyyMMddHHmmss",
+            CultureInfo.InvariantCulture,
+            DateTimeStyles.None
+        );
+    }
+}
