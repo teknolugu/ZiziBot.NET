6ce4e5a10c4cf05618c1b77ef5afbc4fe1033586
Azhe-kun <azhe403@gmail.com>
2022-03-19 22:17:26 +0700

    move ping into core extension

--- src/WinTenDev.Zizi.Services/Telegram/Extensions/TelegramServiceCoreExtension.cs
+++ src/WinTenDev.Zizi.Services/Telegram/Extensions/TelegramServiceCoreExtension.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Reflection;
 using System.Threading.Tasks;
+using SerilogTimings;
+using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
 using WinTenDev.Zizi.Models.Types;
 using WinTenDev.Zizi.Utils;
 using WinTenDev.Zizi.Utils.Telegram;
@@ -9,6 +12,44 @@ namespace WinTenDev.Zizi.Services.Telegram.Extensions;
 
 public static class TelegramServiceCoreExtension
 {
+    public static async Task SendPingAsync(this TelegramService telegramService)
+    {
+        var op = Operation.Begin("Ping Command handler");
+        var chatId = telegramService.ChatId;
+
+        var keyboard = new InlineKeyboardMarkup(InlineKeyboardButton.WithCallbackData("Ping", "PONG"));
+
+        var sendText = "ℹ️ Pong!!";
+
+        var featureConfig = await telegramService.GetFeatureConfig("ping");
+
+        if (telegramService.IsPrivateChat &&
+            telegramService.IsFromSudo ||
+            (featureConfig.AllowsAt?.Contains(chatId.ToString()) ?? false))
+        {
+            sendText += $"\n🕔 <code>{DateTime.Now}</code>" +
+                        "\n🎛 <b>Engine info.</b>";
+            var getWebHookInfo = await telegramService.Client.GetWebhookInfoAsync();
+            if (string.IsNullOrEmpty(getWebHookInfo.Url))
+            {
+                sendText += "\n\n<i>Bot run in Poll mode.</i>";
+            }
+            else
+            {
+                sendText += getWebHookInfo.ParseWebHookInfo();
+            }
+        }
+
+        await telegramService.SendTextMessageAsync(
+            sendText,
+            keyboard,
+            scheduleDeleteAt: DateTime.UtcNow.AddMinutes(1),
+            includeSenderMessage: true
+        );
+
+        op.Complete();
+    }
+
     public static async Task SendAboutAsync(this TelegramService telegramService)
     {
         var enginesConfig = telegramService.EnginesConfig;
--- src/WinTenDev.ZiziBot.AppHost/Handlers/PingHandler.cs
+++ src/WinTenDev.ZiziBot.AppHost/Handlers/PingHandler.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
-using SerilogTimings;
-using Telegram.Bot;
 using Telegram.Bot.Framework.Abstractions;
-using Telegram.Bot.Types.ReplyMarkups;
-using WinTenDev.Zizi.Models.Enums;
 using WinTenDev.Zizi.Services.Telegram;
-using WinTenDev.Zizi.Utils.Telegram;
+using WinTenDev.Zizi.Services.Telegram.Extensions;
 
 namespace WinTenDev.ZiziBot.AppHost.Handlers;
 
@@ -26,36 +21,6 @@ internal class PingHandler : IUpdateHandler
         CancellationToken cancellationToken
     )
     {
-        var op = Operation.Begin("Ping Command handler");
-
-        await _telegramService.AddUpdateContext(context);
-
-        var keyboard = new InlineKeyboardMarkup(InlineKeyboardButton.WithCallbackData("Ping", "PONG"));
-
-        var sendText = "ℹ️ Pong!!";
-        var isSudoer = _telegramService.IsFromSudo;
-
-        _telegramService.SaveSenderMessageToHistory(MessageFlag.Ping);
-
-        if (_telegramService.IsPrivateChat && isSudoer)
-        {
-            sendText += $"\n🕔 <code>{DateTime.Now}</code>" +
-                        "\n🎛 <b>Engine info.</b>";
-            var getWebHookInfo = await _telegramService.Client.GetWebhookInfoAsync(cancellationToken: cancellationToken);
-            if (string.IsNullOrEmpty(getWebHookInfo.Url))
-            {
-                sendText += "\n\n<i>Bot run in Poll mode.</i>";
-            }
-            else
-            {
-                sendText += getWebHookInfo.ParseWebHookInfo();
-            }
-        }
-
-        await _telegramService.SendTextMessageAsync(sendText, keyboard);
-
-        _telegramService.SaveSentMessageToHistory(MessageFlag.Ping);
-
-        op.Complete();
+        await _telegramService.SendPingAsync();
     }
 }
