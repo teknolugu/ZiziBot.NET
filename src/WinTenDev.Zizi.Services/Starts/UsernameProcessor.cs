b96e27ccbfed25e52c7af936c7e2577153271169
Azhe-kun <azhe403@gmail.com>
2022-04-01 22:33:23 +0700

    move start to a core extension

---
+++ src/WinTenDev.Zizi.Services/Starts/UsernameProcessor.cs
@@ -0,0 +1,31 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+using WinTenDev.Zizi.Models.Dto;
+
+namespace WinTenDev.Zizi.Services.Starts
+{
+    public class UsernameProcessor
+    {
+        public MessageResponseDto Execute(string payload)
+        {
+            var response = new MessageResponseDto();
+
+            var replyMarkup = new InlineKeyboardMarkup
+            (
+                new[]
+                {
+                    new[]
+                    {
+                        InlineKeyboardButton.WithUrl("Pasang Username", "https://t.me/WinTenDev/29")
+                    }
+                }
+            );
+
+            var send = "Untuk cara pasang Username, silakan klik tombol di bawah ini";
+
+            response.MessageText = send;
+            response.ReplyMarkup = replyMarkup;
+
+            return response;
+        }
+    }
+}
--- src/WinTenDev.Zizi.Services/Telegram/Extensions/TelegramServiceCoreExtension.cs
+++ src/WinTenDev.Zizi.Services/Telegram/Extensions/TelegramServiceCoreExtension.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 using SerilogTimings;
 using Telegram.Bot;
 using Telegram.Bot.Types.ReplyMarkups;
 using WinTenDev.Zizi.Models.Enums;
 using WinTenDev.Zizi.Models.Types;
+using WinTenDev.Zizi.Services.Starts;
 using WinTenDev.Zizi.Utils;
 using WinTenDev.Zizi.Utils.Telegram;
 
@@ -12,6 +16,89 @@ namespace WinTenDev.Zizi.Services.Telegram.Extensions;
 
 public static class TelegramServiceCoreExtension
 {
+    public static TService GetRequiredService<TService>(this TelegramService telegramService)
+    {
+        var serviceProvider = telegramService.ServiceProvider;
+        var resolvedService = serviceProvider.GetRequiredService<TService>();
+
+        return resolvedService;
+    }
+
+    public static async Task SendStartAsync(this TelegramService telegramService)
+    {
+        var enginesConfig = telegramService.EnginesConfig;
+        var rulesProcessor = telegramService.GetRequiredService<RulesProcessor>();
+        var usernameProcessor = telegramService.GetRequiredService<UsernameProcessor>();
+
+        var msg = telegramService.Message;
+        var partText = msg.Text.SplitText(" ").ToArray();
+        var startArg = partText.ElementAtOrDefault(1);
+        var startArgs = startArg?.Split("_");
+        var startCmd = startArgs?.FirstOrDefault();
+
+        Log.Debug("Start Args: {StartArgs}", startArgs);
+
+        var getMe = await telegramService.BotService.GetMeAsync();
+        var aboutHeader = getMe.GetAboutHeader();
+        var urlStart = await telegramService.BotService.GetUrlStart("start=help");
+        var urlAddTo = await telegramService.BotService.GetUrlStart("startgroup=new");
+
+        var botName = getMe.GetFullName();
+        var botVer = enginesConfig.Version;
+        var botCompany = enginesConfig.Company;
+
+        var winTenDev = botCompany.MkUrl("https://t.me/WinTenDev");
+        var ziziDocs = "https://docs.zizibot.winten.my.id";
+        var levelStandardUrl = $"{ziziDocs}/glosarium/admin-dengan-level-standard";
+        var levelStandard = @"Level standard".MkUrl(levelStandardUrl);
+
+        var sendText = $"🤖 {aboutHeader}" +
+                       $"\nby {winTenDev}." +
+                       $"\n\nAdalah bot debugging dan manajemen grup yang di lengkapi dengan alat keamanan. " +
+                       $"Agar saya bekerja dengan fitur penuh di sebuah Grup, jadikan saya admin dengan {levelStandard}. " +
+                       $"\n\nSaran dan fitur bisa di ajukan di @WinTenDevSupport atau @TgBotID.";
+
+        var result = startCmd switch
+        {
+            "rules" => await rulesProcessor.Execute(startArgs.ElementAtOrDefault(1)),
+            "set-username" => usernameProcessor.Execute(startArgs.ElementAtOrDefault(1)),
+            _ => null
+        };
+
+        if (result != null)
+        {
+            await telegramService.SendTextMessageAsync(
+                sendText: result.MessageText,
+                replyMarkup: result.ReplyMarkup,
+                disableWebPreview: result.DisableWebPreview
+            );
+
+            return;
+        }
+
+        var keyboard = new InlineKeyboardMarkup
+        (
+            new[]
+            {
+                new[]
+                {
+                    InlineKeyboardButton.WithUrl("Bantuan", ziziDocs),
+                    InlineKeyboardButton.WithUrl("Pasang Username", "https://t.me/WinTenDev/29")
+                },
+                new[]
+                {
+                    InlineKeyboardButton.WithUrl("Tambahkan ke Grup", urlAddTo)
+                }
+            }
+        );
+
+        await telegramService.SendTextMessageAsync(
+            sendText: sendText,
+            replyMarkup: keyboard,
+            disableWebPreview: true
+        );
+    }
+
     public static async Task SendPingAsync(this TelegramService telegramService)
     {
         var op = Operation.Begin("Ping Command handler");
