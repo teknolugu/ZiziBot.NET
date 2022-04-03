af999fa673229a28a0563627b198c71e69b5d267
Azhe-kun <azhe403@gmail.com>
2022-04-04 06:53:06 +0700

    add ValueUtil.cs

---
+++ src/WinTenDev.Zizi.Utils/ValueUtil.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace WinTenDev.Zizi.Utils
+{
+    public static class ValueUtil
+    {
+        public static bool IsDefault<T>(this T value) where T : struct
+        {
+            bool isDefault = value.Equals(default(T));
+
+            return isDefault;
+        }
+
+        public static bool IsNull<T, TU>(this KeyValuePair<T, TU> pair)
+        {
+            return pair.Equals(new KeyValuePair<T, TU>());
+        }
+    }
+}
