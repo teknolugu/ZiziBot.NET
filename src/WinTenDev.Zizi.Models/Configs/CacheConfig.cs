1c6b27fb8d8aae8dd1a09b2dfe512d8aae2a24a3
Azhe-kun <azhe403@gmail.com>
2022-04-16 09:17:59 +0700

    use string for expiration cache property

--- src/WinTenDev.Zizi.Models/Configs/CacheConfig.cs
+++ src/WinTenDev.Zizi.Models/Configs/CacheConfig.cs
@@ -2,42 +2,18 @@ using WinTenDev.Zizi.Models.Enums;
 
 namespace WinTenDev.Zizi.Models.Configs;
 
-/// <summary>
-/// Property of Caching Configuration.
-/// </summary>
 public class CacheConfig
 {
-    /// <summary>
-    /// Type of Caching Storage.
-    /// </summary>
     public CacheStorage CacheStorage { get; set; }
-
-    /// <summary>
-    /// If True, cache will be invalidated on start
-    /// </summary>
     public bool InvalidateOnStart { get; set; }
-
-    /// <summary>
-    /// Common cache expiration time
-    /// </summary>
-    public int ExpireAfter { get; set; } = 60;
-
-    /// <summary>
-    /// Gets or sets the value of the stale after
-    /// </summary>
-    public int StaleAfter { get; set; } = 5;
-
-    /// <summary>
-    /// Common known cache prefix, will be invalidated on start.
-    /// </summary>
+    public string ExpireAfter { get; set; } = "60m";
+    public string StaleAfter { get; set; } = "5s";
     public string[] KnownPrefixes { get; set; }
 
-    /// <summary>
-    /// Deconstructs the expire after
-    /// </summary>
-    /// <param name="expireAfter">The expire after</param>
-    /// <param name="staleAfter">The stale after</param>
-    public void Deconstruct(out int expireAfter, out int staleAfter)
+    public void Deconstruct(
+        out string expireAfter,
+        out string staleAfter
+    )
     {
         expireAfter = ExpireAfter;
         staleAfter = StaleAfter;
