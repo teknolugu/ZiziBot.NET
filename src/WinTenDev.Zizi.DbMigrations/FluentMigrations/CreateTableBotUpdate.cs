666c1cd3eb98b8043d959a77f105f7aa0116c0b8
Azhe-kun <azhe403@gmail.com>
2022-04-16 16:23:33 +0700

    save bot updates for public group/channel

---
+++ src/WinTenDev.Zizi.DbMigrations/FluentMigrations/CreateTableBotUpdate.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+using JetBrains.Annotations;
+using WinTenDev.Zizi.DbMigrations.Extensions;
+
+namespace WinTenDev.Zizi.DbMigrations.FluentMigrations;
+
+[Migration(120220416033801)]
+[UsedImplicitly]
+public class CreateTableBotUpdate : Migration
+{
+    private const string TableName = "bot_update";
+
+    public override void Up()
+    {
+        this.CreateTableIfNotExists(
+            TableName,
+            syntax => syntax
+                .WithIdColumn()
+                .WithColumn("bot_name").AsString().NotNullable()
+                .WithTimeStamp()
+                .WithColumn("update").AsMySqlText().NotNullable()
+        );
+    }
+
+    public override void Down()
+    {
+        Execute.DropTableIfExists(TableName);
+    }
+}
---
+++ src/WinTenDev.Zizi.Models/Tables/BotUpdate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RepoDb.Attributes;
+using Telegram.Bot.Types;
+using WinTenDev.Zizi.Models.RepoDb;
+
+namespace WinTenDev.Zizi.Models.Tables;
+
+[Map("bot_update")]
+public class BotUpdate
+{
+    [Primary]
+    public long Id { get; set; }
+
+    [Map("bot_name")]
+    public string BotName { get; set; }
+
+    [PropertyHandler(typeof(UpdatePropertyHandler))]
+    public Update Update { get; set; }
+
+    [Map("created_at")]
+    public DateTime CreatedAt { get; set; }
+}
