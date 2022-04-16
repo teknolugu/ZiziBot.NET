ac3dac77c6e06464876c363e5e5c9c9bfeac84be
Azhe-kun <azhe403@gmail.com>
2022-04-16 16:00:04 +0700

    add debug chat info

---
+++ src/WinTenDev.Zizi.Services/Extensions/TelegramServiceDebugExtension.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using WinTenDev.Zizi.Models.Types;
+using WinTenDev.Zizi.Services.Telegram;
+using WinTenDev.Zizi.Utils;
+using WinTenDev.Zizi.Utils.Telegram;
+
+namespace WinTenDev.Zizi.Services.Extensions;
+
+public static class TelegramServiceDebugExtension
+{
+    public static async Task GetChatInfo(this TelegramService telegramService)
+    {
+        var htmlMessage = HtmlMessage.Empty;
+        var chatId = telegramService.ChatId;
+        var chatService = telegramService.GetRequiredService<ChatService>();
+        var chatParam = telegramService.GetCommandParam(0);
+
+        if (!await telegramService.CheckUserPermission())
+        {
+            await telegramService.DeleteSenderMessageAsync();
+            return;
+        }
+
+        htmlMessage.BoldBr("ℹ Chat Info");
+
+        try
+        {
+            if (chatParam != null) chatId = chatParam.ToInt64().FixChatId();
+            var chatInfo = await chatService.GetChatAsync(chatId);
+            var memberCount = await chatService.GetMemberCountAsync(chatId);
+
+            htmlMessage
+                .Bold("Chat Id: ").CodeBr(chatInfo.Id.ToString())
+                .Bold("Chat Title: ").CodeBr(chatInfo.Title)
+                .Bold("Username: ").CodeBr(chatInfo.Username)
+                .Bold("IsPublic: ").CodeBr((chatInfo.Username != null).ToString())
+                .Bold("Type: ").CodeBr(chatInfo.Type.ToString())
+                .Bold("Linked: ").CodeBr(chatInfo.LinkedChatId.ToString())
+                .Bold("Member: ").CodeBr(memberCount.ToString());
+        }
+        catch (Exception e)
+        {
+            htmlMessage.Text(e.Message);
+        }
+
+        await telegramService.SendTextMessageAsync(
+            sendText: htmlMessage.ToString(),
+            scheduleDeleteAt: DateTime.UtcNow.AddMinutes(10),
+            includeSenderMessage: true
+        );
+    }
+}
