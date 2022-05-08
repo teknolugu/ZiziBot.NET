7420062f5c5e3830e2b5f039b0d7f8cb83610ff5
Azhe-kun <azhe403@gmail.com>
2022-05-08 13:34:58 +0700

    fix db migration

--- src/WinTenDev.Zizi.DbMigrations/FluentMigrations/AlterChatSettingAddFireCheck.cs
+++ src/WinTenDev.Zizi.DbMigrations/FluentMigrations/AlterChatSettingAddFireCheck.cs
@@ -1,5 +1,6 @@
 ﻿using FluentMigrator;
 using JetBrains.Annotations;
+using WinTenDev.Zizi.DbMigrations.Extensions;
 
 namespace WinTenDev.Zizi.DbMigrations.FluentMigrations;
 
@@ -10,9 +11,15 @@ public class AlterChatSettingAddFireCheck : Migration
 
     public override void Up()
     {
-        if (!IfDatabase().Schema.Table(CreateTableChatSettings.TableName).Exists())
-            Alter.Table(CreateTableChatSettings.TableName).AddColumn("enable_fire_check").AsBoolean().WithDefaultValue(0);
+        this.CreateColIfNotExists(
+            CreateTableChatSettings.TableName,
+            "enable_fire_check",
+            syntax => syntax
+                .AsBoolean()
+                .WithDefaultValue(0)
+        );
     }
+
     public override void Down()
     {
         // Nothing to do here
