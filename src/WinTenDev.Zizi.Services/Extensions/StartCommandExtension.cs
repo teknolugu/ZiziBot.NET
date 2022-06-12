a9aac9c5847e7c626038f19562a22e0f9f7e7511
Azhe-kun <azhe403@gmail.com>
2022-06-12 14:56:51 +0700

    run downloading subtitle in background

--- src/WinTenDev.Zizi.Services/Extensions/StartCommandExtension.cs
+++ src/WinTenDev.Zizi.Services/Extensions/StartCommandExtension.cs
@@ -24,61 +24,64 @@ internal static class StartCommandExtension
         var subsceneService = telegramService.GetRequiredService<SubsceneService>();
         var fixedSlug = subtitleSlug.Replace("=", "/");
 
-        await telegramService.SendTextMessageAsync("Subtitle sedang diproses, mohon tunggu...");
+        Task.Run(
+            async () => {
+                await telegramService.SendTextMessageAsync("Subtitle sedang diproses, mohon tunggu...");
+                await telegramService.SendChatActionAsync(ChatAction.UploadDocument);
 
-        await telegramService.SendChatActionAsync(ChatAction.UploadDocument);
+                var movieDetail = await subsceneService.GetSubtitleFileAsync(fixedSlug);
 
-        var movieDetail = await subsceneService.GetSubtitleFileAsync(fixedSlug);
+                var subsceneUrl = "https://subscene.com" + movieDetail.SubtitleMovieUrl;
+                var commentaryUrl = "https://subscene.com" + movieDetail.CommentaryUrl;
+                var serverFileName = await movieDetail.SubtitleDownloadUrl.GetServerFileName();
+                var fileName = movieDetail.ReleaseInfos?
+                                   .OrderBy(s => s.Length).FirstOrDefault(movieDetail.MovieName)?
+                                   .Replace(".", " ") ??
+                               movieDetail.MovieName;
+                var fileNameWithExt = fileName + serverFileName.GetFileExtension();
 
-        var subsceneUrl = "https://subscene.com" + movieDetail.SubtitleMovieUrl;
-        var commentaryUrl = "https://subscene.com" + movieDetail.CommentaryUrl;
-        var serverFileName = await movieDetail.SubtitleDownloadUrl.GetServerFileName();
-        var fileName = movieDetail.ReleaseInfos?
-                           .OrderBy(s => s.Length).FirstOrDefault(movieDetail.MovieName)?
-                           .Replace(".", " ") ??
-                       movieDetail.MovieName;
-        var fileNameWithExt = fileName + serverFileName.GetFileExtension();
+                var subtitleInfo = HtmlMessage.Empty
+                    .Bold("Movie: ").TextBr(movieDetail.MovieName, true)
+                    .Bold("Language: ").TextBr(movieDetail.Language)
+                    .Bold("Author: ").Url(commentaryUrl, movieDetail.CommentaryUser).Br();
 
-        var subtitleInfo = HtmlMessage.Empty
-            .Bold("Movie: ").TextBr(movieDetail.MovieName, true)
-            .Bold("Language: ").TextBr(movieDetail.Language)
-            .Bold("Author: ").Url(commentaryUrl, movieDetail.CommentaryUser).Br();
+                if (movieDetail.ReleaseInfo.IsNotNullOrEmpty())
+                {
+                    subtitleInfo.BoldBr("Release info")
+                        .TextBr(movieDetail.ReleaseInfo, true);
+                }
 
-        if (movieDetail.ReleaseInfo.IsNotNullOrEmpty())
-        {
-            subtitleInfo.BoldBr("Release info")
-                .TextBr(movieDetail.ReleaseInfo, true);
-        }
+                if (movieDetail.Comment.IsNotNullOrEmpty())
+                {
+                    subtitleInfo.Br()
+                        .Text(movieDetail.Comment);
+                }
 
-        if (movieDetail.Comment.IsNotNullOrEmpty())
-        {
-            subtitleInfo.Br()
-                .Text(movieDetail.Comment);
-        }
+                var buttonMarkup = new[]
+                {
+                    new[]
+                    {
+                        InlineKeyboardButton.WithUrl("Tautan Subscene", subsceneUrl),
+                    },
+                    new[]
+                    {
+                        InlineKeyboardButton.WithSwitchInlineQueryCurrentChat("Pencarian baru", "subscene ")
+                    }
+                };
 
-        var buttonMarkup = new[]
-        {
-            new[]
-            {
-                InlineKeyboardButton.WithUrl("Tautan Subscene", subsceneUrl),
-            },
-            new[]
-            {
-                InlineKeyboardButton.WithSwitchInlineQueryCurrentChat("Pencarian baru", "subscene ")
+                var replyMarkup = new InlineKeyboardMarkup(buttonMarkup);
+
+                await telegramService.DeleteSentMessageAsync();
+
+                await telegramService.SendMediaAsync(
+                    fileId: movieDetail.SubtitleDownloadUrl,
+                    mediaType: MediaType.Document,
+                    caption: subtitleInfo.ToString(),
+                    customFileName: fileNameWithExt,
+                    replyMarkup: replyMarkup
+                );
             }
-        };
-
-        var replyMarkup = new InlineKeyboardMarkup(buttonMarkup);
-
-        await telegramService.DeleteSentMessageAsync();
-
-        await telegramService.SendMediaAsync(
-            fileId: movieDetail.SubtitleDownloadUrl,
-            mediaType: MediaType.Document,
-            caption: subtitleInfo.ToString(),
-            customFileName: fileNameWithExt,
-            replyMarkup: replyMarkup
-        );
+        ).InBackground();
 
         return response;
     }
