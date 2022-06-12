e43c191b885b7aa3e5c3e4c6cdd710c5bbdfa8b1
Azhe-kun <azhe403@gmail.com>
2022-06-12 15:33:14 +0700

    update subscene class model

--- src/WinTenDev.Zizi.Models/Entities/MongoDb/SubsceneSubtitleItem.cs
+++ src/WinTenDev.Zizi.Models/Entities/MongoDb/SubsceneSubtitleItem.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Entities;
 
 namespace WinTenDev.Zizi.Models.Entities.MongoDb;
 
-public class SubsceneSubtitleItem : Entity, ICreatedOn, IModifiedOn
+public class SubsceneSubtitleItem : IEntity, ICreatedOn, IModifiedOn
 {
+    [ObjectId, BsonId]
+    public string ID { get; set; }
     public string MovieUrl { get; set; }
     public string MovieName { get; set; }
     public string Language { get; set; }
@@ -12,4 +16,6 @@ public class SubsceneSubtitleItem : Entity, ICreatedOn, IModifiedOn
     public string Comment { get; set; }
     public DateTime CreatedOn { get; set; }
     public DateTime ModifiedOn { get; set; }
+
+    public string GenerateNewID() => ObjectId.GenerateNewId().ToString();
 }
