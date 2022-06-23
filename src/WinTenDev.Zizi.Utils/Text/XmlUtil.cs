bfebf87028ae7fa48fb8d1198f4f6e3c4edfebed
Azhe-kun <azhe403@gmail.com>
2022-06-23 20:43:46 +0700

    add missing XmlUtil

---
+++ src/WinTenDev.Zizi.Utils/Text/XmlUtil.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Linq;
+
+namespace WinTenDev.Zizi.Utils.Text;
+
+public static class XmlUtil
+{
+    public static XElement GetOrCreateElement(
+        this XContainer container,
+        string name
+    )
+    {
+        var element = container.Element(name);
+        if (element != null) return element;
+        element = new XElement(name);
+        container.Add(element);
+        return element;
+    }
+}
