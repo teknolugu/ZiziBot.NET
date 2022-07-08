2f315301ee59024ce5dcd01e6f403cdc84cb621b
Azhe-kun <azhe403@gmail.com>
2022-07-08 13:19:56 +0700

    add IStartupTask interface

---
+++ src/WinTenDev.Zizi.Models/Interfaces/IStartupTask.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace WinTenDev.Zizi.Models.Interfaces;
+
+public interface IStartupTask
+{
+    Task ExecuteAsync(CancellationToken cancellationToken = default);
+}
