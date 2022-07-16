003903dd6b6597e4fbe8c6121b0e9826216b1d4b
Azhe-kun <azhe403@gmail.com>
2022-07-16 19:46:57 +0700

    fix build (2)

---
+++ src/WinTenDev.Zizi.Models/Bots/BotClient.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Options;
+using Telegram.Bot.Framework;
+
+namespace WinTenDev.Zizi.Models.Bots;
+
+public class BotClient : BotBase
+{
+    public BotClient(IOptions<BotOptions<BotClient>> options) : base(options.Value)
+    {
+    }
+}
