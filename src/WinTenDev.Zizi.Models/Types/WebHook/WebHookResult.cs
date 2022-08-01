79ea9b02b19ca50d8aa91faf4aeeae0031046fd8
Azhe-kun <azhe403@gmail.com>
2022-08-01 09:23:35 +0700

    update webhook result class

--- src/WinTenDev.Zizi.Models/Types/WebHook/WebHookResult.cs
+++ src/WinTenDev.Zizi.Models/Types/WebHook/WebHookResult.cs
@@ -1,9 +1,16 @@
-﻿using WinTenDev.Zizi.Models.Enums;
+﻿using System.Text.Json.Serialization;
+using WinTenDev.Zizi.Models.Enums;
 
 namespace WinTenDev.Zizi.Models.Types.WebHook;
 
 public class WebHookResult
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public WebhookSource WebhookSource { get; set; }
+
     public string ParsedMessage { get; set; }
+
+    public string ResponseTime { get; set; }
+
+    public string ExecutionTime { get; set; }
 }
