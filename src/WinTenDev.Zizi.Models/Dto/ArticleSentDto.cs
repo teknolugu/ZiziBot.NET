b4a752f7aeefb685d959d260317cb51f3add6378
Azhe-kun <azhe403@gmail.com>
2022-08-17 23:33:53 +0700

    move rss history to mongodb

---
+++ src/WinTenDev.Zizi.Models/Dto/ArticleSentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WinTenDev.Zizi.Models.Entities.MongoDb.Internal;
+
+public class ArticleSentDto
+{
+    public long ChatId { get; set; }
+    public string RssSource { get; set; }
+    public string Title { get; set; }
+    public string Url { get; set; }
+    public DateTime PublishDate { get; set; }
+    public string Author { get; set; }
+}
