91f0956a6a42e1026de8b59272383347b70cc457
Azhe-kun <azhe403@gmail.com>
2022-08-17 11:41:40 +0700

    add default conventions for mongodb

--- src/WinTenDev.Zizi.Services/Extensions/DatasourceServiceExtension.cs
+++ src/WinTenDev.Zizi.Services/Extensions/DatasourceServiceExtension.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
 using Nito.AsyncEx.Synchronous;
 
 namespace WinTenDev.Zizi.Services.Extensions;
@@ -8,6 +10,11 @@ public static class DatasourceServiceExtension
     public static IApplicationBuilder RunMongoDbPreparation(this IApplicationBuilder app)
     {
         var databaseService = app.GetRequiredService<DatabaseService>();
+
+        ConventionRegistry.Register("DefaultConventions", new ConventionPack
+        {
+            new EnumRepresentationConvention(BsonType.String),
+        }, type => true);
 
         databaseService.MongoDbDatabaseMapping().WaitAndUnwrapException();
         databaseService.MongoDbEnsureCollectionIndex().WaitAndUnwrapException();
