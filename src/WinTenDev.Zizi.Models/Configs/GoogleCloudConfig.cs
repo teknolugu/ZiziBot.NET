b2aebc99464d3dddab8f7ca111024f21e73a51b0
Azhe-kun <azhe403@gmail.com>
2022-09-21 19:18:56 +0700

    update class config property

--- src/WinTenDev.Zizi.Models/Configs/GoogleCloudConfig.cs
+++ src/WinTenDev.Zizi.Models/Configs/GoogleCloudConfig.cs
@@ -3,6 +3,8 @@ namespace WinTenDev.Zizi.Models.Configs;
 public class GoogleCloudConfig
 {
     public string DriveIndexUrl { get; set; }
+    public string DriveIndexWebUrl { get; set; }
+    public string DriveIndexCdnUrl { get; set; }
     public string CredentialsPath { get; set; }
     public string DriveAuth { get; set; }
     public string ZiziBotDrive { get; set; }
