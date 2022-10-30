3f55f722daf6856b6b4db6f0cee9f9e2edfe2ea4
Azhe-kun <azhe403@gmail.com>
2022-10-30 12:52:42 +0700

    add gban user entity

---
+++ src/WinTenDev.Zizi.Models/Entities/MongoDb/Internal/GlobalBanUserEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Entities;
+
+namespace WinTenDev.Zizi.Models.Entities.MongoDb.Internal;
+
+[Collection("GlobalBan_User")]
+public class GlobalBanUserEntity : IEntity, ICreatedOn, IModifiedOn
+{
+    public string GenerateNewID() => ObjectId.GenerateNewId().ToString();
+
+    public string ID { get; set; }
+
+    public long ChatId { get; set; }
+    public long UserId { get; set; }
+    public long BannedUserId { get; set; }
+    public string Reason { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+    public DateTime ModifiedOn { get; set; }
+}
