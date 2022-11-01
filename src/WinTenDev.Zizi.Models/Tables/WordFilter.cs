4d6a2e78357b86db8781022417823cb79b71bbee
Azhe-kun <azhe403@gmail.com>
2022-11-01 16:09:20 +0700

    add field attribute

--- src/WinTenDev.Zizi.Models/Tables/WordFilter.cs
+++ src/WinTenDev.Zizi.Models/Tables/WordFilter.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Text.Json.Serialization;
+using RepoDb.Attributes;
 using SqlKata;
 
 namespace WinTenDev.Zizi.Models.Tables;
 
+[Map("word_filter")]
 public class WordFilter
 {
     [JsonPropertyName("word")]
@@ -12,14 +14,17 @@ public class WordFilter
 
     [JsonPropertyName(("is_global"))]
     [Column("is_global")]
+    [Map("is_global")]
     public bool IsGlobal { get; set; }
 
     [JsonPropertyName("from_id")]
     [Column("from_id")]
+    [Map("from_id")]
     public long FromId { get; set; }
 
     [JsonPropertyName("chat_id")]
     [Column("chat_id")]
+    [Map("chat_id")]
     public long ChatId { get; set; }
 
     [JsonPropertyName("created_at")]
