1c8f4d3df13c15068049c0287f0fae035e25d569
Azhe-kun <azhe403@gmail.com>
2022-11-05 21:41:10 +0700

    register migration

--- src/WinTenDev.Zizi.Services/Internals/DatabaseService.cs
+++ src/WinTenDev.Zizi.Services/Internals/DatabaseService.cs
@@ -309,7 +309,8 @@ public class DatabaseService
             new _002_Game_Initialize_Tebak_Kata_Sample(),
             new _003_GlobalBan_Migrate_From_Mysql(),
             new _004_City_Migrate_From_MySql(),
-            new _005_Word_Migrate_From_MySql()
+            new _005_Word_Migrate_From_MySql(),
+            new _006_Step_Migrate_From_MySql()
         });
     }
 
